165b75ec185a1929f9313a556aa79ddeca0bbb16
Anastasia Malissovas <anastasiamalissovas@gmail.com>
2019-02-25 21:54:16 -0500

    Made game board and styled it.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import axios from 'axios'
 
 class App extends Component {
+  state = {
+    game: [[]]
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://minesweeper-api.herokuapp.com/games', { difficulty: 0 })
+      .then(resp => {
+        this.setState({
+          game: resp.data.board
+        })
+      })
+  }
+
   render() {
-    return <HelloWorld />
+    return (
+      <main>
+        <section>
+          <h1>⭐️Sailor 🎀 Moonswiper🌙</h1>
+          <img
+            src="http://www.picgifs.com/glitter-gifs/s/sailor-moon/picgifs-sailor-moon-79062.gif"
+            id="brooch"
+          />
+          <select>
+            <option>Beginner</option>
+            <option>Intermediate</option>
+            <option>Expert</option>
+          </select>
+          <button className="reset">Reset</button>
+        </section>
+        <section className="gameBody">
+          <table>
+            <tbody>
+              {this.state.game.map(row => {
+                return (
+                  <tr>
+                    {row.map(col => {
+                      // Put an onClick{} event on td for functionality.
+                      return <td>{col}</td>
+                    })}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </section>
+      </main>
+    )
   }
 }
 
